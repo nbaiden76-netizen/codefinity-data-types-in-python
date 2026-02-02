e062a760d3da40055708e2a2d6500a1201c8c4cf
nbaiden76-netizen <>
2026-02-02 22:06:18 +0000

    Division Operators

---
+++ numbers/division_operators/main.py
@@ -0,0 +1,19 @@
+# Calculate the number of completed transactions
+completed = 60 // 7
+# Calculate the number of remaining minutes
+minutes = 60 % 7 
+
+# Print these values
+print("The number of completed transactions is", completed)
+print("The number of remaining minutes is", minutes)
+
+
+# really useful for quick practical division scenarios 
+
+
+# departments = 38000 // 7000
+# print(departments)  # output: 5
+
+# really practical example for when you need to understand the remaining amount avaialble 
+# remaining_budget = 38000 % 7000
+# print(remaining_budget)  # output: 3000
