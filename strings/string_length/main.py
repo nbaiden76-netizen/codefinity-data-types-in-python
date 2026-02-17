d716e46bbe18e6c181cd6f26323f18eec17a5fb6
nbaiden76-netizen <>
2026-02-17 21:29:09 +0000

    String length

---
+++ strings/string_length/main.py
@@ -0,0 +1,15 @@
+string = "My goal is to achieve way beyond what I expected"
+# Extract the very last symbol using negative indexation
+symbol1 = string[-1]
+# Extract the very last symbol using `len()` function
+symbol2 = string[len(string) - 1]
+
+# Print symbols
+print("Variable symbol1 equals:", symbol1)
+print("Variable symbol2 equals:", symbol2)
+
+#string = "Practice is the key to success"
+# length = len(string)
+
+#print("Here is the string:", string)
+#print("And its length is", length)
