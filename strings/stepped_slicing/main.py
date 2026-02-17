2e4781af35ffc3fb73a792599c4ed7744f92f5ab
nbaiden76-netizen <>
2026-02-17 21:23:45 +0000

    Stepped slicing

---
+++ strings/stepped_slicing/main.py
@@ -0,0 +1,9 @@
+filename = "Annual Budget Report 2024"
+
+year_code = filename[21:24:2]
+
+print("Variable sliced_string equals:", year_code)
+
+# code = "TXN-2024-INV-0007"
+# sliced = code[0:13:5]
+# print(sliced)  # Output: T0N
