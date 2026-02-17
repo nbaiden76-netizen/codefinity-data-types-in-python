da680c8b6a1bc4fc0012615b90fa3adc876ec5a2
nbaiden76-netizen <>
2026-02-17 21:33:59 +0000

    Substring search

---
+++ strings/substring_search/main.py
@@ -0,0 +1,17 @@
+string = "Work your way up!"
+
+# Find the index of the letter `p`
+p = string.find('p')
+
+# Find the index of the letter `y`
+y = string.find('y')
+
+# Find the index of the letter `r`
+r = string.find('r')
+
+print("Variable p equals:", p)
+print("Variable y equals:", y)
+print("Variable r equals:", r)
+
+# string = "Channel efforts to success!"
+# print(string.find('n'))
