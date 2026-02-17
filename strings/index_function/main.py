c1fe9edf2ea58ce954c0da97a53654d5cfa3e7a5
nbaiden76-netizen <>
2026-02-17 21:40:34 +0000

    index() Function

---
+++ strings/index_function/main.py
@@ -0,0 +1,22 @@
+string = "It is cool to have a burning desire for studying"
+
+# Find the index of "burning" word
+burning = string.index('burning')
+
+# Find the index of "desire" word
+desire = string.index('desire')
+
+# Find the index of "studying" word
+studying = string.index('studying')
+
+print("The index of the word burning is:", burning)
+print("The index of the word desire is:", desire)
+print("The index of the word studying is:", studying)
+
+# string = "I can manage everything"
+
+# manage = string.index('manage')
+# everything = string.index('everything')
+
+# print("The index of the word manage is", manage)
+# print("The index of the word everything is", everything)
