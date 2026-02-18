d060b4855a26f82a18809725b1a2c4be1b0e3062
nbaiden76-netizen <>
2026-02-18 20:57:11 +0000

    String Immutability

---
+++ strings/string_immutability/main.py
@@ -0,0 +1,25 @@
+string1 = "Rome is the capital of France"
+string2 = "Brasilia is located in Europe"
+string3 = "Monkeys eat earphones"
+
+# Replace substrings to correct the mistakes
+new_string1 = string1.replace('Rome','Paris')
+new_string2 = string2.replace('Europe','South America')
+new_string3 = string3.replace('earphones','bananas')
+
+print(new_string1)
+print(new_string2)
+print(new_string3)
+
+#string = "What does it really take to be first past the post?"
+
+# string[0] = 'k'
+
+#string = "I would like to order seafood"
+# I changed my mind and want to order fish now
+# new_string = string.replace('seafood','fish')
+
+# The previous order
+#print(string)
+# The current order
+# print(new_string)
